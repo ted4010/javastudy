563bd55da0277c94bd51d879b3bc043803be08e1
ted4010 <ted4010@naver.com>
2024-01-04 10:12:09 +0900

    2024/01/04

---
+++ 01_start/src/start/Start.java
@@ -0,0 +1,27 @@
+package start;
+
+public class Start {
+
+  public static void main(String[] args) {
+    
+    // single-line comment
+    
+    /*
+     * multi-line comment
+     */
+    
+    /**
+     * documentation comment
+     * 작정자, 작성일자, 설명 등을 API 문서로 제공하기 위한 주석
+     */
+    
+    // 출력문
+    
+    System.out.println(1);      // println() : 출력 후 줄 바꿈까지 해 준다.
+    System.out.print(2);        // print()   : 출력만 한다.
+    System.out.printf("%d", 3); // printf() : 포맷 문자를 기반으로 출력한다.
+    
+    
+  }
+  
+}
